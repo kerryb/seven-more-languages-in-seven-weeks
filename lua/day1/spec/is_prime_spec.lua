8a09f4044aae4358f2a1818c1e86a58e86eb7c13
Kerry Buckley <kerryjbuckley@gmail.com>
2024-11-08 16:06:48 +0000

    Implement is_prime

---
+++ lua/day1/spec/is_prime_spec.lua
@@ -0,0 +1,43 @@
+describe("is_prime", function()
+  require("is_prime")
+
+  it("is false for 0", function()
+    assert.False(is_prime(0))
+  end)
+
+  it("is false for 1", function()
+    assert.False(is_prime(1))
+  end)
+
+  it("is true for 2", function()
+    assert.True(is_prime(2))
+  end)
+
+  it("is true for 3", function()
+    assert.True(is_prime(3))
+  end)
+
+  it("is false for 4", function()
+    assert.False(is_prime(4))
+  end)
+
+  it("is true for 5", function()
+    assert.True(is_prime(5))
+  end)
+
+  it("is false for 10", function()
+    assert.False(is_prime(10))
+  end)
+
+  it("is true for 11", function()
+    assert.True(is_prime(11))
+  end)
+
+  it("is false for 2^31", function()
+    assert.False(is_prime(2 ^ 31))
+  end)
+
+  it("is true for 2^31 - 1", function()
+    assert.True(is_prime(2 ^ 31 - 1))
+  end)
+end)
---
+++ lua/day1/src/is_prime.lua
@@ -0,0 +1,13 @@
+function is_prime(number)
+  return number == 2 or number > 2 and number % 2 > 0 and has_no_odd_factor_in_range(number, 3, number ^ 0.5)
+end
+
+function has_no_odd_factor_in_range(number, range_start, range_end)
+  if range_start > range_end then
+    return true
+  elseif number % range_start == 0 then
+    return false
+  else
+    return has_no_odd_factor_in_range(number, range_start + 2, range_end)
+  end
+end
