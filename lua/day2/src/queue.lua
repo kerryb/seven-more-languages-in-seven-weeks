20d6ec66dddeb513347774a7c29b834d98642203
Kerry Buckley <kerryjbuckley@gmail.com>
2024-11-15 13:33:33 +0000

    Correctly add and remove items in a Fifo manner

--- lua/day2/src/queue.lua
+++ lua/day2/src/queue.lua
@@ -1,20 +1,21 @@
 local Queue = {}
 
 function Queue:new()
-  local queue = {}
+  -- OK, it’ll overflow after 2^1024 items, but that’s probably fine.
+  local queue = {first = 1, last = 0}
   setmetatable(queue, self)
   self.__index = self
   return queue
 end
 
 function Queue:add(item)
-  self.item = item
+  self[self.last + 1] = item
+  self.last = self.last + 1
 end
 
 function Queue:remove()
-  item = self.item
-  self.item = nil
-  return item
+  self.first = self.first + 1
+  return self[self.first - 1]
 end
 
 return Queue
