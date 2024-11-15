fdc9d16a8237ace50bff89a505063775acb3afd5
Kerry Buckley <kerryjbuckley@gmail.com>
2024-11-15 17:32:33 +0000

    Implement retry to try up to <count> times

--- lua/day2/spec/retry_spec.lua
+++ lua/day2/spec/retry_spec.lua
@@ -4,4 +4,28 @@ describe("Retry.retry", function()
   it("returns the function result if it succeeds (doesn’t yield) immediately", function()
     assert.equals(42, Retry.retry(3, function() return 42 end))
   end)
+
+  it("fails after <count> attempts if the function keeps failing", function()
+    local fun = function()
+      while true do
+        coroutine.yield("nope")
+      end
+    end
+
+    assert.has_error(function() Retry.retry(3, fun) end, "nope")
+  end)
+
+  it("returns successfully if the function succeeds within <count> attempts", function()
+    local count = 1
+    local fun = function()
+      while count < 3 do
+        count = count + 1
+        coroutine.yield("Not there yet!")
+      end
+
+      return "I’ve run " .. count .. " times!"
+    end
+
+    assert.equals("I’ve run 3 times!", Retry.retry(3, fun))
+  end)
 end)
--- lua/day2/src/retry.lua
+++ lua/day2/src/retry.lua
@@ -1,9 +1,17 @@
 local Retry = {}
 
 Retry.retry = function(count, body)
-  job = coroutine.create(body)
-  succeeded, value = coroutine.resume(job)
-  return value
+  local _succeeded, result
+  local job = coroutine.create(body)
+
+  for attempt = 1, count do
+    _succeeded, result = coroutine.resume(job)
+    if coroutine.status(job) == "dead" then
+      return result
+    end
+  end
+
+  error(result)
 end
 
 return Retry
