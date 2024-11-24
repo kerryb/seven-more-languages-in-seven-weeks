9514e565837dc1e341de673ada0a2c63a25cb996
Kerry Buckley <kerryjbuckley@gmail.com>
2024-11-24 22:18:45 +0000

    Repeat four bars

--- lua/day3/play/star_wars.lua
+++ lua/day3/play/star_wars.lua
@@ -2,9 +2,11 @@ song = require "notation"
 
 song.set_tempo(116)
 
-treble = {
+local treble = {
   Rc,
+}
 
+local treble_section = {
   Rm, G4m,
   F4tq, E4tq, D4tq, C5m, G4c,
 
@@ -12,9 +14,14 @@ treble = {
   F4tq, E4tq, F4tq, D4m, Rc,
 }
 
-bass_1 = {
-  G3tq, G3tq, G3tq,
+table.move(treble_section, 1, #treble_section, #treble + 1, treble)
+table.move(treble_section, 1, #treble_section, #treble + 1, treble)
 
+local bass_1 = {
+  G3tq, G3tq, G3tq,
+}
+
+local bass_1_section = {
   C4m, Rm,
   C4c, C4dm,
 
@@ -22,15 +29,23 @@ bass_1 = {
   Bf3c, C4m, G3q, G3q,
 }
 
-bass_2 = {
-  Rc,
+table.move(bass_1_section, 1, #bass_1_section, #bass_1 + 1, bass_1)
+table.move(bass_1_section, 1, #bass_1_section, #bass_1 + 1, bass_1)
 
+local bass_2 = {
+  Rc,
+}
+
+local bass_2_section = {
   Rs,
   A3c, G3dm,
 
   A3c, G3dm,
   Rc, G3m, Rc
 }
+
+table.move(bass_2_section, 1, #bass_2_section, #bass_2 + 1, bass_2)
+table.move(bass_2_section, 1, #bass_2_section, #bass_2 + 1, bass_2)
 
 song.part(treble)
 song.part(bass_1)
