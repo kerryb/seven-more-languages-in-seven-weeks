d4a7015c555398276deb459c0a82539e21fb0e0d
Kerry Buckley <kerryjbuckley@gmail.com>
2025-02-01 16:30:27 +0000

    Show mouse position
    Rupert’s implementation from https://gist.github.com/roovo/ad07ae042015371ba44948a231255fd0

---
+++ elm/day2/mouse.elm
@@ -0,0 +1,78 @@
+module MousePosition exposing (..)
+
+import Browser
+import Browser.Events
+import Html exposing (..)
+import Json.Decode as JD
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Position 0 0, Cmd.none )
+
+
+
+-- MODEL
+
+
+type alias Position =
+    { x : Int, y : Int }
+
+
+type alias Model =
+    Position
+
+
+
+-- UPDATE
+
+
+type Msg
+    = MouseMoved Position
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case ( msg, model ) of
+        ( MouseMoved position, _ ) ->
+            ( position, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Browser.Events.onMouseMove (JD.map MouseMoved positionDecoder)
+
+
+positionDecoder : JD.Decoder Position
+positionDecoder =
+    JD.map2 Position
+        (JD.field "pageX" JD.int)
+        (JD.field "pageY" JD.int)
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text (positionToString model) ]
+
+
+positionToString : Position -> String
+positionToString position =
+    "(" ++ String.fromInt position.x ++ ", " ++ String.fromInt position.y ++ ")"
