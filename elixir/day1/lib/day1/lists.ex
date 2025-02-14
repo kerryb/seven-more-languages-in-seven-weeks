2e4de348a219cd20090f769e3d88a90ec861c002
Kerry Buckley <kerryjbuckley@gmail.com>
2025-02-14 21:22:05 +0000

    Implement Lists.min/1

--- elixir/day1/lib/day1/lists.ex
+++ elixir/day1/lib/day1/lists.ex
@@ -1,4 +1,10 @@
 defmodule Day1.Lists do
   def size([]), do: 0
   def size([_head | tail]), do: 1 + size(tail)
+
+  def min([]), do: raise(ArgumentError, "Empty list")
+  def min([head | tail]), do: do_min(tail, head)
+  defp do_min([], acc), do: acc
+  defp do_min([head | tail], acc) when head < acc, do: do_min(tail, head)
+  defp do_min([_head | tail], acc), do: do_min(tail, acc)
 end
--- elixir/day1/test/day1/lists_test.exs
+++ elixir/day1/test/day1/lists_test.exs
@@ -6,4 +6,14 @@ defmodule Day1.ListsTest do
       assert Day1.Lists.size([1, 2, 3, :foo, nil, [4, 5]]) == 6
     end
   end
+
+  describe "Day1.Lists.min/1" do
+    test "returns the minimum value of a list" do
+      assert Day1.Lists.min([4, 2, 1, 3]) == 1
+    end
+
+    test "raises an exception for an empty list" do
+      assert_raise ArgumentError, "Empty list", fn -> Day1.Lists.min([]) end
+    end
+  end
 end
