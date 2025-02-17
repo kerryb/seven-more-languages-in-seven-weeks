e0d0375f61ed820c6731370ab5699b147e151010
Kerry Buckley <kerryjbuckley@gmail.com>
2025-02-17 20:28:19 +0000

    Check lines properly

--- elixir/day1/lib/day1/tic_tac_toe/board.ex
+++ elixir/day1/lib/day1/tic_tac_toe/board.ex
@@ -1,6 +1,28 @@
 defmodule Day1.TicTacToe.Board do
   defstruct [:nw, :n, :ne, :w, :c, :e, :sw, :s, :se]
 
-  def state(_board, :X), do: :win
-  def state(_board, :O), do: :lose
+  def lines do
+    [
+      [:nw, :n, :ne],
+      [:w, :c, :e],
+      [:sw, :s, :se],
+      [:nw, :w, :sw],
+      [:n, :c, :s],
+      [:ne, :e, :se],
+      [:nw, :c, :se],
+      [:ne, :c, :sw]
+    ]
+  end
+
+  def state(board, player) do
+    Enum.reduce_while(lines(), :playing, fn line, _acc -> check_line(board, line, player) end)
+  end
+
+  defp check_line(board, line, player) do
+    case board |> Map.take(line) |> Map.values() do
+      [^player, ^player, ^player] -> {:halt, :win}
+      [opponent, opponent, opponent] -> {:halt, :lose}
+      _ -> {:cont, :playing}
+    end
+  end
 end
--- elixir/day1/test/day1/tic_tac_toe/board_test.exs
+++ elixir/day1/test/day1/tic_tac_toe/board_test.exs
@@ -2,6 +2,21 @@ defmodule Day1.TicTacToe.BoardTest do
   use ExUnit.Case, async: true
   alias Day1.TicTacToe.Board
 
+  describe "Day1.TicTacToe.Board.lines/0" do
+    test "returns the combinations of positions that constitute a line" do
+      assert Board.lines() == [
+               [:nw, :n, :ne],
+               [:w, :c, :e],
+               [:sw, :s, :se],
+               [:nw, :w, :sw],
+               [:n, :c, :s],
+               [:ne, :e, :se],
+               [:nw, :c, :se],
+               [:ne, :c, :sw]
+             ]
+    end
+  end
+
   describe "Day1.TicTacToe.Board.state/2" do
     test "returns :win if the current player has a line" do
       board =
@@ -11,7 +26,7 @@ defmodule Day1.TicTacToe.BoardTest do
         OOX
         """)
 
-      assert Board.state(board, :X) == :win
+      assert Board.state(board, :x) == :win
     end
 
     test "returns :lose if the other player has a line" do
@@ -22,7 +37,18 @@ defmodule Day1.TicTacToe.BoardTest do
         OOX
         """)
 
-      assert Board.state(board, :O) == :lose
+      assert Board.state(board, :o) == :lose
+    end
+
+    test "returns :playing if neither player has a line" do
+      board =
+        board("""
+        XO.
+        OX.
+        OXO
+        """)
+
+      assert Board.state(board, :o) == :playing
     end
 
     defp board(string) do
