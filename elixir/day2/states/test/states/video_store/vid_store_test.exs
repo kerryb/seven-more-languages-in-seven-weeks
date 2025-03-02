0eba414017d2e76fd4cdc5dba807a926b17dd35f
Kerry Buckley <kerryjbuckley@gmail.com>
2025-03-02 09:05:57 +0000

    Add remaining tests for VidStore

--- elixir/day2/states/test/states/video_store/vid_store_test.exs
+++ elixir/day2/states/test/states/video_store/vid_store_test.exs
@@ -5,36 +5,70 @@ defmodule States.VideoStore.VidStoreTest do
   alias States.VidStore
 
   describe "States.VidStore.rent/1" do
-    test "sets the state to :rented" do
-      rented_video = VidStore.rent(video())
+    setup do
+      %{video: %Video{title: "XMen"}}
+    end
+
+    test "sets the state to :rented", %{video: video} do
+      rented_video = VidStore.rent(video)
       assert rented_video.state == :rented
     end
 
-    test "updates the rental count" do
-      rented_video = VidStore.rent(video())
+    test "updates the rental count", %{video: video} do
+      rented_video = VidStore.rent(video)
       assert rented_video.times_rented == 1
     end
 
-    test "logs the transition" do
-      rented_video = VidStore.rent(video())
-      assert Enum.count(rented_video.log) == 1
+    test "logs the transition", %{video: video} do
+      rented_video = VidStore.rent(video)
+      assert rented_video.log == ["Renting XMen"]
+    end
+  end
+
+  describe "States.VidStore.return/1" do
+    setup do
+      %{video: %Video{state: :rented, title: "XMen"}}
+    end
+
+    test "sets the state to :available", %{video: video} do
+      returned_video = VidStore.return(video)
+      assert returned_video.state == :available
+    end
+
+    test "logs the transition", %{video: video} do
+      returned_video = VidStore.return(video)
+      assert returned_video.log == ["Returning XMen"]
+    end
+  end
+
+  describe "States.VidStore.lose/1" do
+    setup do
+      %{video: %Video{state: :rented, title: "XMen"}}
+    end
+
+    test "sets the state to :lost", %{video: video} do
+      lost_video = VidStore.lose(video)
+      assert lost_video.state == :lost
+    end
+
+    test "logs the transition", %{video: video} do
+      lost_video = VidStore.lose(video)
+      assert lost_video.log == ["Losing XMen"]
     end
   end
 
   describe "States.VidStore" do
     test "handles multiple transitions" do
-      vid =
-        video()
+      video =
+        %Video{title: "XMen"}
         |> VidStore.rent()
         |> VidStore.return()
         |> VidStore.rent()
         |> VidStore.return()
         |> VidStore.rent()
 
-      assert Enum.count(vid.log) == 5
-      assert vid.times_rented == 3
+      assert Enum.count(video.log) == 5
+      assert video.times_rented == 3
     end
   end
-
-  defp video, do: %Video{title: "XMen"}
 end
