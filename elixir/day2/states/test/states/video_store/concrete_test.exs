5faffc504220031f6289e813eb85a8e9c953df24
Kerry Buckley <kerryjbuckley@gmail.com>
2025-03-02 17:27:41 +0000

    Add missing tests to VideoStoreTest

--- elixir/day2/states/test/states/video_store/concrete_test.exs
+++ elixir/day2/states/test/states/video_store/concrete_test.exs
@@ -5,31 +5,70 @@ defmodule States.VideoStore.ConcreteTest do
   alias States.VideoStore.Concrete
 
   describe "States.VideoStore.Concrete.rent/1" do
-    test "sets the state to :rented" do
-      rented_video = Concrete.rent(video())
+    setup do
+      %{video: %Video{title: "XMen"}}
+    end
+
+    test "sets the state to :rented", %{video: video} do
+      rented_video = Concrete.rent(video)
       assert rented_video.state == :rented
     end
 
-    test "logs the transition" do
-      rented_video = Concrete.rent(video())
-      assert Enum.count(rented_video.log) == 1
+    test "updates the rental count", %{video: video} do
+      rented_video = Concrete.rent(video)
+      assert rented_video.times_rented == 1
+    end
+
+    test "logs the transition", %{video: video} do
+      rented_video = Concrete.rent(video)
+      assert rented_video.log == ["Renting XMen"]
+    end
+  end
+
+  describe "States.VideoStore.Concrete.return/1" do
+    setup do
+      %{video: %Video{state: :rented, title: "XMen"}}
+    end
+
+    test "sets the state to :available", %{video: video} do
+      returned_video = Concrete.return(video)
+      assert returned_video.state == :available
+    end
+
+    test "logs the transition", %{video: video} do
+      returned_video = Concrete.return(video)
+      assert returned_video.log == ["Returning XMen"]
+    end
+  end
+
+  describe "States.VideoStore.Concrete.lose/1" do
+    setup do
+      %{video: %Video{state: :rented, title: "XMen"}}
+    end
+
+    test "sets the state to :lost", %{video: video} do
+      lost_video = Concrete.lose(video)
+      assert lost_video.state == :lost
+    end
+
+    test "logs the transition", %{video: video} do
+      lost_video = Concrete.lose(video)
+      assert lost_video.log == ["Losing XMen"]
     end
   end
 
   describe "States.VideoStore.Concrete" do
     test "handles multiple transitions" do
-      vid =
-        video()
+      video =
+        %Video{title: "XMen"}
         |> Concrete.rent()
         |> Concrete.return()
         |> Concrete.rent()
         |> Concrete.return()
         |> Concrete.rent()
 
-      assert Enum.count(vid.log) == 5
-      assert vid.times_rented == 3
+      assert Enum.count(video.log) == 5
+      assert video.times_rented == 3
     end
   end
-
-  defp video, do: %Video{title: "XMen"}
 end
