2ead295aefdd5a156fea78fde369d2326685dddf
Kerry Buckley <kerryjbuckley@gmail.com>
2025-03-21 11:09:30 +0000

    Remove original non-macro version of VideoStore

--- elixir/day2/states/lib/states/video_store/video_store.ex
+++ elixir/day2/states/lib/states/video_store/video_store.ex
@@ -1,17 +1,3 @@
 defmodule States.VideoStore do
   @moduledoc false
-  def renting(video) do
-    vid = log(video, "Renting #{video.title}")
-    %{vid | times_rented: video.times_rented + 1}
-  end
-
-  def returning(video), do: log(video, "Returning #{video.title}")
-
-  def losing(video), do: log(video, "Losing #{video.title}")
-
-  def finding(video), do: log(video, "Finding #{video.title}")
-
-  defp log(video, message) do
-    %{video | log: [message | video.log]}
-  end
 end
