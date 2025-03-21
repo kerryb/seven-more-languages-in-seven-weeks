93bd9c13c71592d69721a08cd29728a43e937862
Kerry Buckley <kerryjbuckley@gmail.com>
2025-03-21 11:12:58 +0000

    Add top-level API for video store (not in the book)

--- elixir/day2/states/lib/states/video_store/server.ex
+++ elixir/day2/states/lib/states/video_store/server.ex
@@ -10,17 +10,20 @@ defmodule States.VideoStore.Server do
     GenServer.start_link(__MODULE__, videos, name: :video_store)
   end
 
+  @impl true
   def init(videos) do
     {:ok, videos}
   end
 
+  @impl true
+  def handle_cast({:add, video}, videos) do
+    {:noreply, [video | videos]}
+  end
+
+  @impl true
   def handle_call({action, item}, _from, videos) do
     video = videos[item]
     new_video = apply(VidStore, action, [video])
     {:reply, new_video, Keyword.put(videos, item, new_video)}
-  end
-
-  def handle_cast({:add, video}, videos) do
-    {:noreply, [video | videos]}
   end
 end
--- elixir/day2/states/lib/states/video_store/video_store.ex
+++ elixir/day2/states/lib/states/video_store/video_store.ex
@@ -1,3 +1,9 @@
 defmodule States.VideoStore do
   @moduledoc false
+
+  def add(video), do: GenServer.cast(:video_store, {:add, video})
+  def rent(video), do: GenServer.call(:video_store, {:rent, video})
+  def return(video), do: GenServer.call(:video_store, {:return, video})
+  def lose(video), do: GenServer.call(:video_store, {:lose, video})
+  def find(video), do: GenServer.call(:video_store, {:find, video})
 end
