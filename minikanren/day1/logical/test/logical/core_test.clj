86b96d3c615f0c09ed36d387e3f388574b8f2c28
Kerry Buckley <kerryjbuckley@gmail.com>
2025-06-02 13:53:32 +0100

    Rename test to match function

--- minikanren/day1/logical/test/logical/core_test.clj
+++ minikanren/day1/logical/test/logical/core_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [logical.core :refer :all]))
 
-(deftest turing-test ; not that one
+(deftest turing-scientists-test
   (testing "turing finds all turing award winning scientists"
     (is (=
          '(:barbara-liskov :frances-allen :john-mccarthy :leslie-lamport)
